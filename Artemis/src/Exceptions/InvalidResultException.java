a2a67a53180880c24e96f9638ed878db0baca8b6
yashutanna <tanna.yashu@gmail.com>
2015-01-28 16:14:58 +0200

    implemented GameResultFactory

---
+++ Artemis/src/Exceptions/InvalidResultException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+/**
+ * Created by yashu.tanna on 2015-01-28.
+ */
+public class InvalidResultException extends Exception {
+}
--- Artemis/src/Game/Engine/GameManager.java
+++ Artemis/src/Game/Engine/GameManager.java
@@ -1,5 +1,20 @@
 package Game.Engine;
 
-public class GameManager {
+import GameState.GameState;
+import Renderer.CLIRenderer;
+import Renderer.IRenderer;
 
+public class GameManager {
+    private GameState gamestate;
+    IRenderer renderer;
+    public void initialise()
+    {
+        gamestate = new GameState();
+        renderer = new CLIRenderer();
+
+        renderer.displayWelcomeScreen();
+        Boolean computer = renderer.confirmComputerPlayer();
+        String PlayerName = renderer.requestPlayerName();
+
+    }
 }
