d3295bb78c75ac749d8f6097ff0f858e7ec89612
yashutanna <tanna.yashu@gmail.com>
2015-01-28 12:59:29 +0200

    aded player and computer player objects

---
+++ Artemis/src/Game/Player/HumanPlayer.java
@@ -0,0 +1,13 @@
+package Game.Player;
+
+import Game.Moves.Move;
+
+/**
+ * Created by yashu.tanna on 2015-01-28.
+ */
+public class HumanPlayer implements IPlayer {
+    @Override
+    public Move getMove() {
+        return null;
+    }
+}
