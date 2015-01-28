6691bb6f1645c2dac2e08af7e59aba58c14bb2bc
yashutanna <tanna.yashu@gmail.com>
2015-01-28 13:37:37 +0200

    changed TextFileMoveSource object

--- Artemis/src/Game/Moves/Move.java
+++ Artemis/src/Game/Moves/Move.java
@@ -8,4 +8,15 @@ public class Move {
     public Move(String name){
         moveName = name;
     }
+
+    @Override
+    public boolean equals(Object other)
+    {
+        if(other.getClass() != this.getClass())
+            return false;
+        Move o = (Move) other;
+        if(o.moveName != this.moveName)
+            return false;
+        return true;
+    }
 }
