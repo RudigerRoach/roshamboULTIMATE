e17f3ab8e20e3ad9ebbfcf47fb0112b1d9c0a290
Edward Steere <edward.steere@entelect.co.za>
2015-01-28 13:52:56 +0200

    Initial commit of TextFileRuleSource, MovePairKey and TextFileRuleSource

---
+++ Artemis/src/Game/Engine/MovePairKey.java
@@ -0,0 +1,13 @@
+package Game.Engine;
+
+public class MovePairKey
+{
+    private Move move1;
+    private Move move2;
+
+    public MovePairKey(Move move1, Move move2)
+    {
+        this.move1 = move1;
+        this.move2 = move2;
+    }
+}
--- Artemis/src/Game/Engine/TextFileRuleSource.java
+++ Artemis/src/Game/Engine/TextFileRuleSource.java
@@ -2,17 +2,49 @@ package Game.Engine;
 
 import Game.Moves.Move;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class TextFileRuleSource implements IRuleSource
 {
-    private File physicalRuleSource;
+    private Map<MovePairKey, IGameResult> rules;
     
-    public TextFileRuleSource(string fileName)
+    public TextFileRuleSource(string fileName, IMoveSource moveSource)
     {
-        physicalRuleSource = new File(fileName);
+        File               ruleFile           = new File(fileName);
+        FileReader         ruleFileReader     = new FileReader(ruleFile);
+        BufferedFileReader physicalRuleSource = new BufferedFileReader(ruleFileReader);
+        String             ruleLine;
+        rules = new HashMap<MovePairKey, IGameResult>();
+
+        while ((ruleLine = physicalRuleSource.readLine()) != null)
+        {
+            String[]    values = ruleLine.split(",");
+            Move        move1  = moveSource.getMoveFromName(values[0]);
+            Move        move2  = moveSource.getMoveFromName(values[1]);
+            MovePairKey key    = new MovePairKey(move1, move2);
+            rules.put(key, new Rule(values[2]));
+        }
     }
     
     public IGameResult applyRule(Move move1, Move move2)
     {
-        // TODO
+        MovePairKey movePairKey = new MovePairKey(move1, move2);
+        if (containsKey(movePairKey))
+        {
+            return rules.get(movePairKey);
+        }
+        
+        MovePairKey movePairKeyReversed = new MovePairKey(move2, move1);
+        if (!containsKey(movePairKey))
+        {
+            throw new MovePairNotDefinedException();
+        }
+
+        return rules.get(movePairKey);
     }
 }
