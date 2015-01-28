2d4d3e2bf71fa01fab94ff036f04f64dc47429b3
yashutanna <tanna.yashu@gmail.com>
2015-01-28 15:12:32 +0200

    added overrriden HashCode method for overriding equals method on Move onject

--- Artemis/src/Game/Engine/TextFileMoveSource.java
+++ Artemis/src/Game/Engine/TextFileMoveSource.java
@@ -11,17 +11,23 @@ import java.util.Map;
 public class TextFileMoveSource implements IMoveSource {
     File MoveFile;
     Map<String, Move> moveMap;
-
+    String filename;
     @Override
     public Move getMoveFromName(String name) {
         return moveMap.get(name);
     }
 
-    public TextFileMoveSource(String filename) throws IOException, EmptyFileException
+    public TextFileMoveSource(String _filename) throws IOException, EmptyFileException
     {
-        MoveFile = new File(filename);
+        MoveFile = new File(_filename);
+        filename = _filename;
         moveMap = new HashMap<String, Move>();
         generateMoveObjects();
+    }
+
+    public String getFileName()
+    {
+        return filename;
     }
 
     private void generateMoveObjects() throws IOException, EmptyFileException {
